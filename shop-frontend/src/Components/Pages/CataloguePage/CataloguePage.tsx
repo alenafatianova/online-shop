f394a97fc7b09571c2e08790538659a607134504
Alena Fatianova <ravemybody@gmail.com>
2024-04-10 20:54:24 +0200

    Add breadcrumbs from antd

--- shop-frontend/src/Components/Pages/CataloguePage/CataloguePage.tsx
+++ shop-frontend/src/Components/Pages/CataloguePage/CataloguePage.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './CataloguePage.css'
 import banner from '../../../assets/Banner_texture.png'
+import { Breadcrumb } from 'antd'
+import { NavLink } from 'react-router-dom'
 
 export const CataloguePage = () => {
+    
+    const breadcrumbsItems = [
+        {
+          title: <NavLink to="/">Главная</NavLink>,
+        },
+        {
+          title: <NavLink to="/catalogue">Каталог</NavLink>,
+        },
+    ]
+
   return (
     <div className='catalogue_wrapper'>
         <div className='catalogue_banner'>
@@ -11,6 +23,7 @@ export const CataloguePage = () => {
             <p className='banner-text_first-row'>подходящую именно Вам</p>
             <p className='banner-text_second-row'>Подходят для лица и тела</p>
         </div>
+       <Breadcrumb className='breadcrumbs' items={breadcrumbsItems} />
     </div>
   )
 }
